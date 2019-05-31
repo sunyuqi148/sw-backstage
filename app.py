2e2ed8ce717a6e05f4f67331d897e64e1880942a
alice-rin <39362322+alice-rin@users.noreply.github.com>
2019-05-31 15:50:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -656,6 +656,6 @@ def test():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=80, debug=True, ssl_context='adhoc')
 #    app.run(host='127.0.0.1', port=5000, debug=True)
     
